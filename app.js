aeeda510da06965bae7fe61b593bd3dc996f20b6
rschalo <reed0schalo@gmail.com>
2020-04-20 11:46:38 -0700

    refactor: use route promises in campgrounds route

--- app.js
+++ app.js
@@ -23,10 +23,7 @@ mongoose.connect(process.env.MONGODB_URI || process.env.DEVDB_URI, {
   useNewUrlParser: true,
 }, () => { console.log('DB successfully connected')}).catch(err => console.log(err))
 
-/*mongoose.connect("mongodb://localhost/yelp_camp_v3", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});*/
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + `/public`));
