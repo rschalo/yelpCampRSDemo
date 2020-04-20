2c4dc8ba219df4908eaaca487538b162aaa4c7df
rschalo <reed0schalo@gmail.com>
2020-04-20 10:27:24 -0700

    test: use cs routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -46,7 +46,7 @@ router
     res.redirect("/");
     console.log("Added " + newCampground["name"]);
   });
-*/
+
 router.get('/', function(req, res) {
   Campground.find({}, function(err, allCampgrounds) {
     if (err) {
@@ -54,11 +54,47 @@ router.get('/', function(req, res) {
     } else {
       res.render(`campgrounds/index`, { campgrounds: allCampgrounds })
     }
-  })
-})
+  });
+});
+*/
+
+//Colt get route
+router.get("/", function(req, res){	
+  // Get all campgrounds from DB	
+  Campground.find({}, function(err, allCampgrounds){	
+     if(err){	
+         console.log(err);	
+     } else {	
+        res.render("campgrounds/index",{campgrounds:allCampgrounds});	
+     }	
+  });	
+});
+
+//Colt Create Route
+router.post("/", middleware.isLoggedIn, function(req, res){	
+  // get data from form and add to campgrounds array	
+  var name = req.body.name;	
+  var image = req.body.image;	
+  var desc = req.body.description;	
+  var author = {	
+      id: req.user._id,	
+      username: req.user.username	
+  }	
+  var newCampground = {name: name, image: image, description: desc, author:author}	
+  // Create a new campground and save to DB	
+  Campground.create(newCampground, function(err, newlyCreated){	
+      if(err){	
+          console.log(err);	
+      } else {	
+          //redirect back to campgrounds page	
+          console.log(newlyCreated);	
+          res.redirect("/campgrounds");	
+      }	
+  });	
+});
 
 
-//campground new route
+//campground get new route
 router.get("/new", middleware.isLoggedIn, function(req, res) {
   res.render("campgrounds/new");
 });
