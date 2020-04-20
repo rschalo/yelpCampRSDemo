f48a99526f822a3d6a5f945aeda874dab4443c49
rschalo <reed0schalo@gmail.com>
2020-04-20 10:40:12 -0700

    fix: call connectDB method

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express = require("express"),
+var express = require('express'),
   app = express(),
-  bodyParser = require("body-parser"),
-  mongoose = require("mongoose"),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
   passport = require(`passport`),
   LocalStrategy = require(`passport-local`),
-  Campground = require("./models/campground"),
+  Campground = require('./models/campground'),
   Comment = require(`./models/comment`),
   User = require(`./models/user`),
-  seedDB = require("./seeds"),
+  seedDB = require('./seeds'),
   methodOverride = require(`method-override`);
 flash = require(`connect-flash`);
 
@@ -19,7 +19,8 @@ var commentRoutes = require(`./routes/comments`),
 //seedDB();
 
 //implement mongoose promise error fix from https://github.com/Automattic/mongoose/issues/8373
-const URI = 'mongodb+srv://rschalo:0rqJdPYxA5hVjujU@cluster0-pfvxq.mongodb.net/test?retryWrites=true&w=majority'
+const URI =
+  'mongodb+srv://rschalo:0rqJdPYxA5hVjujU@cluster0-pfvxq.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.Promise = global.Promise;
 
@@ -27,23 +28,23 @@ const connectDB = async () => {
   mongoose.connect(URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useCreateIndex: true
-  })
-  console.log('DB Connected...')
-}
+    useCreateIndex: true,
+  });
+  console.log('DB Connected...');
+};
 
 /*mongoose.connect("mongodb://localhost/yelp_camp_v3", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });*/
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + `/public`));
 app.use(
   require(`express-session`)({
     secret: `Zoey is my favorite dog in the whole world`,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
 );
 app.use(methodOverride(`_method`));
@@ -54,7 +55,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.locals.currentUser = req.user;
   res.locals.error = req.flash(`error`);
   res.locals.success = req.flash(`success`);
@@ -65,8 +66,8 @@ app.use(`/`, indexRoutes);
 app.use(`/campgrounds/:id/comments`, commentRoutes);
 app.use(`/campgrounds`, campgroundRoutes);
 
-
+connectDB();
 const Port = process.env.Port || 3000;
-app.listen(Port, function(req, res) {
-  console.log("YelpCamp server is live on ", Port);
+app.listen(Port, function (req, res) {
+  console.log('YelpCamp server is live on ', Port);
 });
